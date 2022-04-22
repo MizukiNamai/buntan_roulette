533ab597619533249effde42051e005bb3c6f526
mizuki namai <nms1x3de@gmail.com>
2022-04-22 14:24:43 +0900

    Fix:Lintチェックの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_scope :user do
-    get "login", :to => "users/sessions#new"
-    get "logout", :to => "users/sessions#destroy"
+    get 'login', to: 'users/sessions#new'
+    get 'logout', to: 'users/sessions#destroy'
   end
   # Defines the root path route ("/")
   # root "articles#index"
