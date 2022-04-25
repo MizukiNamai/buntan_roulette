fd698c284dcf84ae23d8a495755571699c77e3c6
mizuki namai <nms1x3de@gmail.com>
2022-04-25 17:15:08 +0900

    Add:人数選択ページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   root 'pages#top'
+
+  get 'pages/choice'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_scope :user do
     get 'login', to: 'users/sessions#new'
