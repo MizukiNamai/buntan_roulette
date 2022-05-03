bb9a6ab481e07797cee1e559ea4eee965ae051c0
mizuki namai <nms1x3de@gmail.com>
2022-05-04 00:07:37 +0900

    Fix:一括入力フォームのcontrollerを修正

--- app/controllers/roulettes_controller.rb
+++ app/controllers/roulettes_controller.rb
@@ -1,5 +1,15 @@
 class RoulettesController < ApplicationController
-  def new; end
+
+  def new
+      @form1 = params[:form1]
+      @form2 = params[:form2]
+      @form3 = params[:form3]
+      @form4 = params[:form4]
+      @form5 = params[:form5]
+      @form6 = params[:form6]
+      @form7 = params[:form7]
+      @form8 = params[:form8]
+  end
 
   def create; end
 end
