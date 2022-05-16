6839d5e629225750e67a6aae0194004ae96e1a9d
mizuki namai <nms1x3de@gmail.com>
2022-05-16 20:53:42 +0900

    Fix:rouletteコントローラーの修正

--- app/controllers/roulettes_controller.rb
+++ app/controllers/roulettes_controller.rb
@@ -1,6 +1,6 @@
 class RoulettesController < ApplicationController
   def new
-    hash = params[:formdate]
+    hash = formdate_params
     array = hash.values
     (1..array.size).each do |i|
       binding.eval("@form#{i} = array[#{i}]")
@@ -8,4 +8,10 @@ class RoulettesController < ApplicationController
   end
 
   def create; end
+
+  private
+
+  def formdate_params
+    params.require(:formdate).permit(:form1, :form2, :form3, :form4, :form5, :form6, :form7, :form8)
+  end
 end
