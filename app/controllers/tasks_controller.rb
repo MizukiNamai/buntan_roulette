d8c3dd3bcf53b5e888101f93e14b3da5668d833f
mizuki namai <nms1x3de@gmail.com>
2022-06-05 23:14:48 +0900

    Add:入力フォーム反映のCSS変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -31,6 +31,6 @@ class TasksController < ApplicationController
   end
 
   def formdate_params
-    params.require(:formdate).permit(:form1, :form2, :form3, :form4, :form5, :form6, :form7, :form8, :form9, :form10)
+    params.require(:formdate).permit(:form1, :form2, :form3, :form4, :form5, :form6, :form7, :form8, :form9, :form10, :form11, :form12, :form13, :form14, :form15, :form16, :form17, :form18, :form19, :form20)
   end
 end
