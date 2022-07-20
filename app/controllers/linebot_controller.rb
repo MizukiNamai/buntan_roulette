d6e2748685ba4a229ce93d977a41dd8a0e5b3976
mizuki namai <nms1x3de@gmail.com>
2022-07-20 16:30:08 +0900

    Fix:Lineチェックの修正

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -2,10 +2,10 @@ class LinebotController < ApplicationController
   require 'line/bot'  # gem 'line-bot-api'
 
   def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
+    @client ||= Line::Bot::Client.new do |config|
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
+    end
   end
 
   def push
@@ -13,8 +13,8 @@ class LinebotController < ApplicationController
     limit_tasks = Task.where(user_uid: current_user.uid, page_id: page).where(updated_at: Time.now - 60.minutes...Time.now)
     limit_tasks.each do |t|
       message = {
-          type: 'text',
-          text: "「#{t.name}」:「#{t.task}」"
+        type: 'text',
+        text: "「#{t.name}」:「#{t.task}」"
       }
       response = client.push_message(t.user_uid, message)
       p response
